4cc818a7b915ae4e89614b361d8f6b5c7ad95327
zavashas <simazavadskis2808@gmail.com>
2024-01-30 14:25:11 +0300

    swjkdl

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    double a, b;
+
+    cout << "Введите значение переменной a: ";
+    cin >> a;
+    double* p_a = &a;
+    *p_a *= 3;
+    cout << "Увеличенное в 3 раза значение переменной a: " << a << endl;
+
+    cout << "Введите значение переменной b: ";
+    cin >> b;
+    double* p_b = &b;
+    *p_b /= 3;
+    cout << "Уменьшенное в 3 раза значение переменной b: " << b << endl;
+
+    return 0;
+}
